3d9dda8b808c871b737501de40e6d6a4602ba7da
Victor Ferreira Silva <mail@victorf.ch>
2022-10-16 20:52:41 +0100

    outra alteracao

--- helloworld.py
+++ helloworld.py
@@ -9,7 +9,7 @@ print('Hoje é dia', datetime.datetime.now().day, 'de', datetime.datetime.now().
 def get_date_from_day_number_of_year(day_number_of_year, year):
     return datetime.date(year, 1, 1) + datetime.timedelta(day_number_of_year - 1)
 
-day_number = int(input('Digite o dia do ano: '))
+day_number = int(input('Por favor, digite o dia do ano: '))
 year = int(input('Digite o ano: '))
 
 print(get_date_from_day_number_of_year(day_number, year))
