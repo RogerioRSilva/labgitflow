bb548a3c3c13ca123a31a6175300d6d292292258
Victor Ferreira Silva <mail@victorf.ch>
2022-10-16 20:34:17 +0100

    adicionadas mais funcionalidades para demonstracao

--- helloworld.py
+++ helloworld.py
@@ -1,3 +1,13 @@
+import datetime
+
 nome = input('Digite seu nome: ')
 
 print(nome)
+
+def get_date_from_day_number_of_year(day_number_of_year, year):
+    return datetime.date(year, 1, 1) + datetime.timedelta(day_number_of_year - 1)
+
+day_number = int(input('Digite o dia do ano: '))
+year = int(input('Digite o ano: '))
+
+print(get_date_from_day_number_of_year(day_number, year))
