1633cfb77525c3354146c13191afc9d403361b09
RogerioRSilva <114150848+RogerioRSilva@users.noreply.github.com>
2022-10-16 16:52:56 -0300

    Remoção de comentários e alteração no código.

--- helloworld.py
+++ helloworld.py
@@ -1,21 +1,9 @@
 import datetime
+from telnetlib import STATUS
 
 nome = input('Digite seu nome: ')
 
 print('123456789')
-
-'''
-4645465 4654 654 4  4654654
-
-
-gjdfogjdfjgdkfjglkdfgdf
-gd
-g
-df
-df
-gd
-'''
-
 
 print(nome)
 
@@ -24,7 +12,7 @@ print('Hoje é dia', datetime.datetime.now().day, 'de', datetime.datetime.now().
 def get_date_from_day_number_of_year(day_number_of_year, year):
     return datetime.date(year, 1, 1) + datetime.timedelta(day_number_of_year - 1)
 
-day_number = int(input('Digite o dia do ano: '))
+day_number = int(input('Digite o dia do ano, por favor: '))
 year = int(input('Digite o ano: '))
 
 print(get_date_from_day_number_of_year(day_number, year))
